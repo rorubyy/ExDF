5f4819ac24eff993dfdebfe06edb8bdd46c258c6
rorubyy <roruby@gmail.com>
2024-05-10 14:49:39 +0800

    add evaluation file

--- load_model.py
+++
@@ -1,7 +0,0 @@
-# Load model directly
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-tokenizer = AutoTokenizer.from_pretrained("eachadea/vicuna-7b-1.1")
-model = AutoModelForCausalLM.from_pretrained("eachadea/vicuna-7b-1.1")
-# from transformers import file_utils
-# print(file_utils.default_cache_path)
